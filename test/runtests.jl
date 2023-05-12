68b6bd354b95ae34672432b21f252eea9099c025
Isaac Wheeler <ickaser@gmail.com>
2023-05-12 10:29:51 -0400

    Amend WENO tests in a minor way

--- test/runtests.jl
+++ test/runtests.jl
@@ -59,18 +59,18 @@ dϕdz_4_anl[1,1] = dϕdz_4_anl[1,2] # Avoid singularity
 
     @test sum(approxzero.(dϕdx_all_1[1])) == dom.ntot  # East derivatives: 0
     @test sum(approxzero.(dϕdx_all_1[2])) == dom.ntot  # West derivatives: 0
-    @test sum(dϕdx_all_1[3] .≈ 1) == dom.ntot # North derivatives: 1 
-    @test sum(dϕdx_all_1[4] .≈ 1) == dom.ntot # South derivatives: 1 
+    @test sum(dϕdx_all_1[3] .≈ 1) == dom.ntot-dom.nr # North derivatives: 1 
+    @test sum(dϕdx_all_1[4] .≈ 1) == dom.ntot-dom.nr # South derivatives: 1 
 
-    @test sum(dϕdx_all_2[1] .≈ 1) == dom.ntot  # East derivatives: 1
-    @test sum(dϕdx_all_2[2] .≈ 1) == dom.ntot  # West derivatives: 1
+    @test sum(dϕdx_all_2[1] .≈ 1) == dom.ntot-dom.nz  # East derivatives: 1
+    @test sum(dϕdx_all_2[2] .≈ 1) == dom.ntot-dom.nz  # West derivatives: 1
     @test sum(approxzero.(dϕdx_all_2[3])) == dom.ntot  # North derivatives: 0
     @test sum(approxzero.(dϕdx_all_2[4])) == dom.ntot  # South derivatives: 0
 
     @test sum(isapprox.(dϕdx_all_4[1], dϕdr_4_anl, atol=dom.dr)) >= dom.ntot - dom.nz # North derivatives: allow for breakage at north boundary
-    @test sum(isapprox.(dϕdx_all_4[2], dϕdr_4_anl, atol=dom.dr)) >= dom.ntot - dom.nz # South derivatives: allow for breakage at south boundary
+    @test sum(isapprox.(dϕdx_all_4[2], dϕdr_4_anl, atol=dom.dr)) >= dom.ntot - dom.nz - 5 # South derivatives: break at south edge, center discontinuity
     @test sum(isapprox.(dϕdx_all_4[3], dϕdz_4_anl, atol=dom.dz)) >= dom.ntot - dom.nr # East derivatives : allow for breakage at east boundary
-    @test sum(isapprox.(dϕdx_all_4[4], dϕdz_4_anl, atol=dom.dz)) >= dom.ntot - dom.nr # West derivatives : allow for breakage at west boundary
+    @test sum(isapprox.(dϕdx_all_4[4], dϕdz_4_anl, atol=dom.dz)) >= dom.ntot - dom.nr - 5 # West derivatives : allow for breakage at west boundary, center discontinuity
 
     @test_broken sum(isapprox.(dϕdx_all_4[1], dϕdx_all_4[2], atol=dom.dr)) == dom.ntot # East and west are within grid size
     @test_broken sum(isapprox.(dϕdx_all_4[3], dϕdx_all_4[4], atol=dom.dz)) == dom.ntot # North and south are within grid size
@@ -91,6 +91,7 @@ surf_post = LSS.compute_icesurf(ϕ_post, dom)
     @test vol_pre ≈ vol_post rtol = 1/max(dom.nr, dom.nz)^2 # Second order mass conservation
     @test surf_pre ≈ surf_post rtol = 1/max(dom.nr, dom.nz)^2 # Second order area conservation
 end
+
 
 @testset "T solution: nothing here" begin
     # Compare to analytical:
@@ -115,6 +116,9 @@ end
 end
 
 @testset "p derivatives for velocity: no tests yet" begin
+end
+
+@testset "velocity extrapolation" begin
 end
 
 ti = time() - ti
