08a47b3a781055b2e99a0922fdd7c6d605233b9e
Isaac Wheeler <ickaser@gmail.com>
2024-12-11 15:44:19 +0100

    Cruft cleanup

--- src/physical_data.jl
+++ src/physical_data.jl
@@ -5,11 +5,9 @@
 # Sublimation pressure, to more significant figures than necessary but hey
 # Left without units becuase this is used inside functions
 
-
 # Triple point, without units: from IAPWS 1995, as rereleased in 2018
 const psub_Tt = 273.16 # Triple point T, kelvin
 const psub_pt = 611.654_771 # Triple point p, Pa
-
 
 const psub_ei = [
  20.996_966_510_789_7,
@@ -57,15 +55,3 @@ const Cp_sucrose = 422.50u"J/mol/K" / 342.3u"g/mol"
 # Not clear what temperature (or even state) this is for
 const k_mannitol1 = 0.60u"W/m/K"
 const k_mannitol2 = 0.74u"W/m/K"
-
-# Typical value of porosity, heuristically
-# const ϵ_typical = 0.92
-
-# Guessed values of porous structure parameters
-const l_guess = 1e-5 * u"m" # 10 micron pore size
-const κ_guess = 1e-10 * u"m^2" # 100 Darcys
-
-# Typical value at around pch = 20 Pa
-const Kshf_guess = 15.0u"W/m^2/K"
-const pch_guess = 20.0u"Pa"
-
