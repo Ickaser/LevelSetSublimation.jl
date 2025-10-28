5756e2f8fd5101ad6a1dad02a54ae47a4a8551d0
Isaac Wheeler <ickaser@gmail.com>
2025-10-28 13:31:57 -0400

    actually deploy the docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+
 using LevelSetSublimation
 using Documenter
 using DocumenterCitations
@@ -5,6 +7,7 @@ using DocumenterCitations
 bib = CitationBibliography(joinpath((@__DIR__), "bib", "Zotero_LevelSet.bib");
         style=:authoryear)
 
+@info "Building Documentation"
 makedocs(
         sitename="Level Set Sublimation Documentation",
         pages = [
@@ -22,4 +25,20 @@ makedocs(
         ]
         ; plugins = [bib], 
         )
+
+if CI
+    @info "Deploying Documentation"
+    deploydocs(
+        # `repo` MUST be set correctly. Once your GitHub name is set
+        # the auto-generated documentation will be hosted at:
+        # https://PutYourGitHubNameHere.github.io/LyoPronto.jl/dev/
+        # (assuming you have enabled `gh-pages` deployment)
+        repo = "github.com/ickaser/LevelSetSublimation.jl.git",
+        target = "build",
+        push_preview = true,
+        devbranch = "main",
+    )
+end
+
+@info "Finished with Documentation"
 
